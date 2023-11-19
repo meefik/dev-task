88ce2bd24b87f4de2961a1a578b96ab6d8af9fcb
Anton Skshidlevskiy <meefik@gmail.com>
2023-11-20 00:59:24 +0100

    Prepare task

--- public/app.js
+++ public/app.js
@@ -1,14 +1,3 @@
 window.onload = () => {
-  const list = document.getElementById('list')
-  const search = document.getElementById('search')
-  search.onkeyup = async (e) => {
-    const res = await fetch(`/api/search?filter=${search.value}`)
-    const countries = await res.json()
-    list.innerHTML = ''
-    for (const country of countries) {
-      const li = document.createElement('li')
-      li.textContent = `${country.name} (${country.code})`
-      list.appendChild(li)
-    }
-  }
+  console.log('loaded')
 }
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
-const countries = require('./countries.json')
 
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
 
-app.get('/api/search', (req, res) => {
-  const filter = req.query.filter
-  const re = new RegExp(filter, 'i')
-  res.json(countries.filter(item => re.test(item.name)).slice(0, 10))
+app.get('/api/hello', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
